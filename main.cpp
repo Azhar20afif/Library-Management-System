0071a39a39d7df1b7301df8ab083494bbbcd5b4e
Azhar20afif <124029366+Azhar20afif@users.noreply.github.com>
2023-12-28 01:11:28 +0500

    alot of changes

--- main.cpp
+++ main.cpp
@@ -3,68 +3,127 @@
 
 using namespace std;
 
-struct Student {
-   string Name;
-   int Reg;
+// Node for linked list
+struct StudentNode {
+    string name;
+    int reg;
+    StudentNode* next;
 };
 
-class Listnode {
+// Node for binary search tree
+struct BookNode {
+    string title;
+    string author;
+    BookNode* left;
+    BookNode* right;
+};
+
+class LibraryManagementSystem {
 public:
-   Student st;
-   Listnode *next;
+    StudentNode* studentList;
+    BookNode* bookTree;
 
-   Listnode(Student s) {
-       st = s;
-       next = NULL;
-   }
+public:
+    LibraryManagementSystem() : studentList(nullptr), bookTree(nullptr) {}
+
+    // Function to add a student to the linked list
+    void addStudent(string name, int reg) {
+        StudentNode* newNode = new StudentNode{ name, reg, nullptr };
+        // Add at the beginning of the list for simplicity
+        newNode->next = studentList;
+        studentList = newNode;
+    }
+
+    // Function to display students using merge sort
+    // (Merge sort implementation not included for brevity)
+    void displayStudents() {
+        // Implement merge sort to sort students by name
+        // Display the sorted list
+    }
+
+    // Function to remove a student from the linked list
+    void removeStudent(int reg) {
+        // Implement code to remove a student by registration number
+    }
+
+    // Function to add a book to the binary search tree
+    void addBook(string title, string author) {
+        bookTree = insertBook(bookTree, title, author);
+    }
+
+    // Recursive function to insert a book into the BST
+    BookNode* insertBook(BookNode* root, string title, string author) {
+        if (root == nullptr) {
+            return new BookNode{ title, author, nullptr, nullptr };
+        }
+
+        if (title < root->title) {
+            root->left = insertBook(root->left, title, author);
+        } else if (title > root->title) {
+            root->right = insertBook(root->right, title, author);
+        }
+
+        return root;
+    }
+
+    // Function to display books using inorder traversal
+    void displayBooks(BookNode* root) {
+        if (root != nullptr) {
+            displayBooks(root->left);
+            cout << "Title: " << root->title << ", Author: " << root->author << endl;
+            displayBooks(root->right);
+        }
+    }
+
+    // Function to delete a book from the binary search tree
+    void deleteBook(string title) {
+        bookTree = deleteBookNode(bookTree, title);
+    }
+
+    // Recursive function to delete a book from the BST
+    BookNode* deleteBookNode(BookNode* root, string title) {
+        // Implement code to delete a book by title
+        return root;
+    }
+
+    // Other functions like checkOutBook and returnBook can be added as needed
 };
-
-void insertStudent(Listnode** head, Student student) {
-   Listnode* newNode = new Listnode(student);
-
-   if (*head == nullptr) {
-       *head = newNode;
-       return;
-   }
-
-   Listnode* current = *head;
-   while (current->next != nullptr) {
-       current = current->next;
-   }
-   current->next = newNode;
-}
-
-Student getStudentInfo() {
-   string name;
-   int reg;
-
-   cout << "Enter student name (or 'quit' to exit): ";
-   getline(cin, name);
-
-   if (name == "quit") {
-       exit(0);
-   }
-
-   cout << "Enter student registration number: ";
-   cin >> reg;
-
-   return {name, reg};
-}
-
-void addStudentToList(Listnode* array[26]) {
-   Student student = getStudentInfo();
-   int index = tolower(student.Name[0]) - 'a';
-   insertStudent(&array[index], student);
-   cout << "Student added successfully!\n";
-   cin.ignore();
-}
 
 int main() {
-   Listnode* array[26] = {nullptr};
+    LibraryManagementSystem library;
 
-   while (true) {
-       addStudentToList(array);
-   }
+    // Adding students
+    library.addStudent("John Doe", 123);
+    library.addStudent("Alice Smith", 456);
+    library.addStudent("Bob Johnson", 789);
 
-   return 0;
+    // Displaying students
+    cout << "Students:" << endl;
+    library.displayStudents();
+
+    // Adding books
+    library.addBook("Introduction to Programming", "Jane Doe");
+    library.addBook("Data Structures and Algorithms", "Bob Smith");
+    library.addBook("Database Management", "Alice Johnson");
+
+    // Displaying books
+// Displaying books
+cout << "\nBooks:" << endl;
+   library.displayBooks(library.bookTree);
+
+
+    // Removing a student
+    library.removeStudent(456);
+
+    // Deleting a book
+    library.deleteBook("Data Structures and Algorithms");
+
+    // Displaying updated students and books
+    cout << "\nUpdated Students:" << endl;
+    library.displayStudents();
+
+    cout << "\nUpdated Books:" << endl;
+    library.displayBooks(library.bookTree);
+
+    return 0;
 }
